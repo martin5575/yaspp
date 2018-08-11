35cf45b81b25b59f2593d26e1356389f13b3e84c
martin5575 <martin.kalvelage@gmx.de>
2018-08-11 11:03:19 +0200

    Simplify header

--- yaspp-react-redux/src/Header.jsx
+++ yaspp-react-redux/src/Header.jsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { Component } from 'react'
-import logo from './img/logo.png'
 
 class Header extends Component {
   constructor(props) {
@@ -8,21 +7,11 @@ class Header extends Component {
   }
 
   render() {
-    const logoWidth = Math.min(window.innerWidth * 0.1, 100) //705
-    const logoHeight = 655 * (logoWidth / 705)
     return (
       <header>
         <div className="container">
           <div className="row">
-            <div className="col-xs-2 col-xs-1-push">
-              <img
-                src={logo}
-                alt="logo"
-                height={logoHeight}
-                width={logoWidth}
-              />
-            </div>
-            <div className="col-xs-8">
+            <div className="col-xs-12 text-center">
               <div style={{ fontSize: 'x-large' }}>Yet Another Sports Page</div>
             </div>
           </div>
