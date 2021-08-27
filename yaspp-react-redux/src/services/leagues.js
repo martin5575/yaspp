ea3bd8669672083433212e47937130d6f6f5849c
Martin kline Kalvelage <martin.kalvelage@gmx.de>
2021-08-27 10:49:46 +0200

    update years in source

--- yaspp-react-redux/src/services/leagues.js
+++ yaspp-react-redux/src/services/leagues.js
@@ -3,14 +3,14 @@ const leagues = [
     id: 'bl1',
     name: '1.Bundesliga',
     key: 3005,
-    years: [2018, 2017, 2003],
+    years: [2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015]
   },
   {
     id: 'bl2',
     name: '2.Bundesliga',
-    years: [2018, 2017, 2016, 2015],
+    years: [2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015]
   },
-  { id: 'bl3', name: '3.Bundesliga', years: [2018, 2017, 2016, 2015] },
+  { id: 'bl3', name: '3.Bundesliga', years: [2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015] },
   { id: 'fifa18', name: 'WM18', years: [2018] },
 ]
 
