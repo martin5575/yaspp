8a2fb268d98555a6876252a3fbffb03eaa129042
martin5575 <martin.kalvelage@gmx.de>
2023-09-16 11:33:28 +0200

    build

---
+++ yaspp-react-redux/build/precache-manifest.01a83c4a2729041ab693ae17621b72fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9ac736acef2db85a765",
+    "url": "./static/css/main.ff357a2a.chunk.css"
+  },
+  {
+    "revision": "e9ac736acef2db85a765",
+    "url": "./static/js/main.5c8c10fb.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5a4be6d1752ceae4598f",
+    "url": "./static/js/2.72ddad06.chunk.js"
+  },
+  {
+    "revision": "9b950267154c133a7e2b775ca555b6d6",
+    "url": "./index.html"
+  }
+];
