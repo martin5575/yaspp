cfb5f4c9301a77f292a85bc83aa5dd746767c935
martin5575 <mkk@ankaadia.com>
2023-10-03 02:08:09 +0200

    build

---
+++ yaspp-react-redux/build/precache-manifest.8496ea1fc83afd417bf6f3be4b437d26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e3c8aee896a6cc66650",
+    "url": "./static/css/main.457d8a09.chunk.css"
+  },
+  {
+    "revision": "5e3c8aee896a6cc66650",
+    "url": "./static/js/main.5e3c8aee.chunk.js"
+  },
+  {
+    "revision": "59f9c340169d13aadf08",
+    "url": "./static/js/1.59f9c340.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "08606070f826f4be03fe34d07ba9316d",
+    "url": "./index.html"
+  }
+];
--- yaspp-react-redux/build/service-worker.js
+++ yaspp-react-redux/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.01a83c4a2729041ab693ae17621b72fe.js"
+  "./precache-manifest.8496ea1fc83afd417bf6f3be4b437d26.js"
 );
 
 workbox.clientsClaim();
