2d5e2501d1608da66225922fbe308cb0bb54b36a
martin5575 <mkk@ankaadia.com>
2023-11-25 13:16:25 +0100

    fix dropdown

--- yaspp-react-redux/src/components/ListNavigator.jsx
+++ yaspp-react-redux/src/components/ListNavigator.jsx
@@ -1,24 +1,29 @@
-import * as React from 'react'
-import { Component } from 'react'
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Button } from 'reactstrap'
+import { Button, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap'
 
-class ListNavigator extends Component {
-  onSelect(event) {
-    let node = event.target
-    while (!node.id) {
-      node = node.parentElement
+
+  function ListNavigator(props) {
+
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggle = () => setDropdownOpen((prevState) => !prevState);
+
+    const onSelect = (event) => {
+      if (!event) return;
+      let node = event?.target
+      while (!node?.id) {
+        node = node.parentElement
+      }
+      props.onSelect(node.id)
     }
-    this.props.onSelect(node.id)
-  }
 
-  render() {
-    if (!this.props.data) return <div />
-    const buttonStyles = this.props.buttonStyles ? this.props.buttonStyles : ''
-    const bgStyles = this.props.bgStyles ? this.props.bgStyles : ''
+    if (!props.data) return <div />
+    const buttonStyles = props.buttonStyles ? props.buttonStyles : ''
+    const bgStyles = props.bgStyles ? props.bgStyles : ''
 
-    const data = this.props.data
-    const currentIndex = data.findIndex((x) => x.id === this.props.selected)
+    const data = props.data
+    const currentIndex = data.findIndex((x) => x.id === props.selected)
     const selectedItem = data[currentIndex]
     const nextIndex = currentIndex + 1
     const prevIndex = currentIndex - 1
@@ -26,6 +31,7 @@ class ListNavigator extends Component {
     const prevId = prevIndex >= 0 ? data[prevIndex].id : undefined
 
     return (
+      <div className="btn-group" role="group">
       <div
         className={`btn-group ${bgStyles}`}
         role="group"
@@ -36,36 +42,28 @@ class ListNavigator extends Component {
           id={prevId}
           className={`btn btn-secondary ${buttonStyles}`}
           disabled={prevId === undefined}
-          onClick={this.onSelect.bind(this)}
+          onClick={e=>onSelect(e)}
         >
           <FontAwesomeIcon icon="caret-left" />
         </Button>
-        <div className="btn-group" role="group">
-          <Button
-            id="btnGroupDrop1"
-            className={`btn btn-secondary dropdown-toggle ${buttonStyles}`}
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            {selectedItem ? selectedItem.name : this.props.selected}
-          </Button>
-          <div className="dropdown-menu" aria-labelledby="btnGroupDrop1">
-            {this.props.data.map((x) => (
-              <Button
-                className="dropdown-item"
-                id={x.id}
-                key={x.id}
-                onClick={this.onSelect.bind(this)}
-              >
-                {x.name}
-              </Button>
-            ))}
-          </div>
+          <Dropdown isOpen={dropdownOpen} toggle={toggle} direction="down">
+            <DropdownToggle caret>{selectedItem ? selectedItem.name : props.selected}</DropdownToggle>
+            <DropdownMenu>
+              {props.data.map((x) => (
+                <DropdownItem
+                  id={x.id}
+                  key={x.id}
+                  onClick={e=>onSelect(e)}
+                >
+                  {x.name}
+                </DropdownItem>
+              ))}
+            </DropdownMenu>
+          </Dropdown>
           <Button
             id={nextId}
             className={`btn btn-secondary ${buttonStyles}`}
-            onClick={this.onSelect.bind(this)}
+            onClick={e=>onSelect(e)}
             disabled={nextId === undefined}
           >
             <FontAwesomeIcon icon="caret-right" />
@@ -74,5 +72,5 @@ class ListNavigator extends Component {
       </div>
     )
   }
-}
+
 export default ListNavigator
